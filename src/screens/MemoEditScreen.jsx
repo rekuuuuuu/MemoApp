78259214c4f1ff080a34edb062e763867fef8dcb
Rkkk <67547614+rekuuuuuu@users.noreply.github.com>
2023-04-30 13:55:59 +0900

    Connect screens with react-navigation

---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, TextInput, StyleSheet, Alert } from "react-native";
+
+import CircleButton from "../components/CircleButton";
+import KeyboardSafeView from "../components/KeyboardSafeView";
+
+export default function MemoEditScreen(props) {
+  const { navigation } = props;
+  return (
+    <KeyboardSafeView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput value="買い物リスト" multiline style={styles.input}></TextInput>
+      </View>
+      <CircleButton
+        name="check"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      ></CircleButton>
+    </KeyboardSafeView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: "top",
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
